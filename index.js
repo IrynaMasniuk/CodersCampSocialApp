b77fb6c9b858dde03dbdafdafdae4d40ad4b909c
Mikołaj <zacczlowiek@gmail.com>
2019-12-10 16:20:08 +0100

    Added Comments
    I added comments - post, get, put, delete. Not sure about this. Better check it before merging.

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const logger = require('./middleware/logger');
 const users = require('./routes/users');
 const friends = require('./routes/friends');
 const express = require('express');
+const comments = require('./routes/comments');
 // const express = require('./database');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true})); 
 app.use('/api/users', users);
-app.use('/api.users/:id/friends',friends) 
+app.use('/api.users/:id/friends',friends) ;
+app.use('/?', comments);              //Nie wiem gdzie będą komentarze...
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}... `))
