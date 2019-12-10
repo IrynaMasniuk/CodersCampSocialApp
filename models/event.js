31a0abb2069b68018ffe27d39b974902815fbb65
Michał Gorgolewski <gorgolewskimichal@gmail.com>
2019-12-10 16:38:04 +0100

    events

--- models/event.js
+++ models/event.js
@@ -6,14 +6,30 @@ const Event = mongoose.model('Customer', new mongoose.Schema({
         type: String,
         required: true,
         minlength: 5,
-        maxlength: 50
+        maxlength: 100
     },
+    date: {
+        type: Date,
+        required: true
+    },
+    place: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 100
+    },
+    description: {
+        type: String
+    }
 
 }));
 
 function validateEvent(event) {
     const schema = {
-        name: Joi.string().min(5).max(50).required(),
+        name: Joi.string().min(5).max(100).required(),
+        date: Joi.date().required(),
+        place: Joi.string().min(5).max(100).required(),
+        description: Joi.string()
     };
 
     return Joi.validate(event, schema);
