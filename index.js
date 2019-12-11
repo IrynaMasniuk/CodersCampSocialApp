e7c870384438b43295f0263486eeff9675e45c2b
Michał Gorgolewski <gorgolewskimichal@gmail.com>
2019-12-11 19:33:32 +0100

    events

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const Joi = require('joi');
+const Joi = require('joi')
 const logger = require('./middleware/logger');
 const users = require('./routes/users');
 const friends = require('./routes/friends');
+const events = require('./routes/events')
 const express = require('express');
-const comments = require('./routes/comments');
 // const express = require('./database');
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true})); 
-app.use('/api/users', users);
-app.use('/api/users/:id/friends',friends); 
-app.use('/?', comments);              //Nie wiem gdzie będą komentarze...
 
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use('/api/users', users);
+app.use('/api.users/:id/friends', friends);
+app.use('/api/events', events);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}... `))
---
+++ routes/events.js
@@ -0,0 +1,63 @@
+const {
+    Event,
+    validate
+} = require('../models/event');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+// const eventManager = new EventManagerModule();
+
+
+// router.get('/', async (req, res) => {
+//     const events = await Event.find().sort('name')
+//     res.send(events);
+// });
+
+router.post('/', async (req, res) => {
+    const {
+        error
+    } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let event = new Event({
+        name: req.body.name
+    })
+    event = await event.save();
+
+    res.send(event);
+});
+
+router.put('/:id', async (req, res) => {
+    const {
+        error
+    } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const event = await Event.findByIdAndUpdate(req.params.id, {
+        name: req.body.name
+    }, {
+        new: true
+    });
+
+    if (!event) return res.status(404).send('The event with the given ID was not found.');
+
+    res.send(event);
+});
+
+router.delete('/:id', async (req, res) => {
+    const event = await Event.findByIdAndRemove(req.params.id);
+
+    if (!event) return res.status(404).send('The event with the given ID was not found.');
+
+    res.send(event);
+});
+
+router.get('/:id', async (req, res) => {
+    const event = await Event.findById(req.params.id);
+
+    if (!event) return res.status(404).send('The event with the given ID was not found.');
+
+    res.send(event);
+});
+
+module.exports = router;
