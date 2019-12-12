3ec7aae40162afc158f19c137264b09209f6f889
Iryna Masniuk <rodzynia@gmail.com>
2019-12-12 12:02:58 +0100

    Revert "events"
    This reverts commit e7c870384438b43295f0263486eeff9675e45c2b.

--- routes/friends.js
+++ routes/friends.js
@@ -1,40 +1,51 @@
-const Joi = require('joi');
-const FriendsModule = require('../friendsmanager');
+const {Friend, createFriend, validateFriend} = require('../Models/friend');
+const mongoose = require('mongoose');
 const app = require('express');
 const router = app.Router();
-const friendManager = new FriendsModule();
 
 
-router.get('/api.users/:id/friends/:id', (req,res)=>{
-    const friend = friends.find(c => c.id === parseInt(req.params.id));
-    if(!friend) res.status(404).send('This person is not in your friends list'); 
-    res.send(friend)
+router.get('/', async (req,res)=>{
+    const friends = await Friend.find().sort('username');
+    res.send(friends);
 });
 
-router.get('/api.users/:id/friends', (req,res)=>{
-    res.send(friends);
+router.get('/:id', async (req,res)=>{
+    const friend = await Friend.findById(req.params.id);
+    if(!friend) return res.status(404).send('This user is not on your friends list');
+    res.send(friend);
 })
 
-router.post('/api.users/:id/friends',  async (req, res) => {
+router.post('/',  async (req, res) => {
     const { error } = validateFriend(req.body);
     if (error) return res.status(400).send(error.details[0].message);
    
-    const friend = await friendManager.createFriend(req.body);
-    console.log('new: ' + friend);
+    const friend = await Friend.findById(req.params.id)
+    if(!friend) return friend = await createFriend(req.body);
+    if(friend) return res.status(404).send('This person you already have as a friend');
+
     res.send(friend);
 });
 
-function validateFriend(friend){
-    console.log(JSON.stringify(friend));
-const schema = {
-    username: Joi.string().min(2).max(50).required(),
-    dateOfBirth: Joi.string().required(),
-    email: Joi.string().min(5).max(255).required(),
-    typeofFriend: Joi.any().valid('best friend', 'friend', 'familiar', 'colleague', 'relative'),
-    riendshipState: Joi.boolean()
-};
-return Joi.validate(friend, schema);
-};
+router.put('/:id', async(req,res)=>{
+    const { error } = validateFriend(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    
+    const friend = await Friend.findByIdAndUpdate(req.params.id,{
+        typeofFriend: req.body.typeofFriend
+    },{new: true});
+
+    res.send(friend);
+});
+
+router.delete('/:id', async (req,res)=>{
+    const friend = await Friend.findByIdAndRemove(req.params.id);
+
+    if(!friend) return res.status(404).send('This user is not on your friends list');
+    
+    res.send(friend);
+});
+
+
 
 
 
