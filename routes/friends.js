6380d1b3cf0c1361cdd727e94bcb227f2c614b70
Elithabet <elizyshchuk@gmail.com>
2019-12-14 14:10:24 +0100

    Addition

--- routes/friends.js
+++ routes/friends.js
@@ -4,25 +4,25 @@ const app = require('express');
 const router = app.Router();
 
 router.post('/', async (req,res)=>{
-const user = await users.User.findById(req.body.user_id);
+const user = await users.User.findById(req.params.user_id);
 if (!user) return res.status(404).send('The user with the given ID was not found.');
 
-user = await user.listOfFriends.push(req.body.friend_id);
+user = await user.listOfFriends.push(req.params.friend_id);
 res.send(user.listOfFriends)
 });
 
 router.get('/', async (req,res)=>{
- const user = await users.User.findById(req.body._id);
+ const user = await users.User.findById(req.params._id);
  if (!user) return res.status(404).send('The user with the given ID was not found.');
 
  res.send(user.listOfFriends)
 });
 
 router.delete('/', async (req,res)=>{
-const user = await users.User.findById(req.body.user_id);
+const user = await users.User.findById(req.params.user_id);
 if (!user) return res.status(404).send('The user with the given ID was not found.');
 
-const friend = await user.listOfFriends.findByIdAndRemove(req.body.friend_id);
+const friend = await user.listOfFriends.findByIdAndRemove(req.params.friend_id);
 
 //const friend = await user.listOfFriends.findById(req.body.friend_id);
 if (!friend) return res.status(404).send('The friend with the given ID was not found.');
