a58d69d00421044f168a2014405a968ef854a2cd
Michał Gorgolewski <gorgolewskimichal@gmail.com>
2020-01-05 01:05:12 +0100

    index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Joi = require('joi');
+const cors = require('cors');
 const comments = require('./routes/comments');
 const events = require('./routes/events');
 const friends = require('./routes/friends');
@@ -6,7 +7,7 @@ const posts = require('./routes/posts');
 const bodyParser = require('body-parser');
 const users = require('./routes/users');
 const express = require('express');
-
+const events = require('./routes/events')
 const app = express();
 
 app.use(express.json());
@@ -14,11 +15,13 @@ app.use(express.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
+app.use(cors());
+
 app.use('/api/users', users);
 app.use('/api/friends', friends);
-app.use('/api/events', events);
-app.use('/api/comments', comments);   
-app.use('/api/posts', posts);   
+app.use('/api', events);
+app.use('/api/comments', comments);
+app.use('/api/posts', posts);
 
 
 const port = process.env.PORT || 5000;
